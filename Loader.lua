67cc5294ae5967646fa276efbf8d157939eba4f6
Hermanos <89389178+hermanos-dev@users.noreply.github.com>
2026-09-13 03:30:59 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,11 +1,11 @@
 -- local script_mode = "PVP" -- PVP, FARM
 local scripts = {
     [6765805766] = { -- Block Spin
-        PVP  = "https://api.luarmor.net/files/v3/loaders/f35c34bae1acf8c1422df5214310b8eb.lua",
-        FARM = "https://api.luarmor.net/files/v3/loaders/32b520fd3ef340a3c6f701b6edf48e55.lua",
+        PVP  = "https://api.luarmor.net/files/v4/loaders/f35c34bae1acf8c1422df5214310b8eb.lua",
+        FARM = "https://api.luarmor.net/files/v4/loaders/32b520fd3ef340a3c6f701b6edf48e55.lua",
     },
     [994732206] = { -- Blox Fruits
-        PVP = "https://api.luarmor.net/files/v3/loaders/341aeb3eaa42e6423a4cdbf2b148e77f.lua",
+        PVP = "https://api.luarmor.net/files/v4/loaders/341aeb3eaa42e6423a4cdbf2b148e77f.lua",
     }
 }
 
